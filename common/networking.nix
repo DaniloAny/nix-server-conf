bf33deb159c777c7a79ac2be6338baebdab6b616
Main user <daniloany@proton.me>
2026-02-02 21:16:23 +0100

    added static ip

--- common/networking.nix
+++ common/networking.nix
@@ -1,8 +1,16 @@
 { config, ... }:
 {
   networking = {
+    useDHCP = false;
     firewall.allowedUDPPorts = [ 22 ];
     hostName = "homeserver";
+    interfaces.enp0s25.ipv4.addresses = [{
+      address = "192.168.0.69";
+      prefixLenght = 24;
+    }];
+    defaultGateway = "192.168.0.1";
+    interfaces.enp0s25.useDHCP = false;
+    nameservers = "8.8.8.8";
   };
 
   # ssh config
