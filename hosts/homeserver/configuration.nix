bce106f2176390498a41a586eb67afa8490f0122
Main user <daniloany@proton.me>
2026-02-03 14:45:10 +0100

    added pihole

--- hosts/homeserver/configuration.nix
+++ hosts/homeserver/configuration.nix
@@ -3,6 +3,7 @@
   imports = [
     ./hardware-configuration.nix
     "${mods}/tailscale.nix"
+    "${mods}/pihole.nix"
   ];
   system.stateVersion = "25.05";
 }
---
+++ modules/pihole.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+services.pihole-ftl = {
+
+    enable = true;
+    openFirewallDNS = true;              # To open port 53 for DNS traffic
+
+    # Settings documented at <https://docs.pi-hole.net/ftldns/configfile/>
+    settings = {
+        dns.upstreams = [ "1.1.1.1" ];   # To use Cloudflare's DNS Servers
+    };
+
+    # Lists can be added via URL
+    lists = [
+        {
+            url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt";
+            type = "block";
+            enabled = true;
+            description = "Sample blocklist by hagezi";
+        }
+    ];
+};
+}
