d58c3089139e8e779279d735a4f16b4676d46c15
Main user <daniloany@proton.me>
2026-02-17 17:32:09 +0100

    im actually stupid

--- modules/navidrome.nix
+++ modules/navidrome.nix
@@ -8,6 +8,6 @@
             Address = "192.168.0.69";
         };
     };
-    firewall.allowedTCPPorts = [ 4533 ];
-    firewall.allowedUDPPorts = [ 4533 ];
+    netowrking.firewall.allowedTCPPorts = [ 4533 ];
+    networking.firewall.allowedUDPPorts = [ 4533 ];
 }
