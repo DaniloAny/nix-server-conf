73b9f19ac6dee8627ee388b3f37ecce7ed3b94d9
Main user <daniloany@proton.me>
2026-02-21 19:23:12 +0100

    this is a commit

--- modules/navidrome.nix
+++ modules/navidrome.nix
@@ -3,11 +3,12 @@
     services.navidrome = {
         enable = true;
         settings = {
-            MusicFolder = "./music/";
+            MusicFolder = "/home/admin/nix-server-conf/modules/music";
             Port = 4533;
             Address = "192.168.0.69";
         };
     };
     networking.firewall.allowedTCPPorts = [ 4533 ];
     networking.firewall.allowedUDPPorts = [ 4533 ];
+    systemd.services.navidrome.serviceConfig.ProtectHome = "read-only";
 }
