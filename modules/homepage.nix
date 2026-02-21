f09712763d5cbd370aeaa4fb8a99f6287608cbfe
Main user <daniloany@proton.me>
2026-02-21 10:47:14 +0100

    something something

--- modules/homepage.nix
+++ modules/homepage.nix
@@ -5,7 +5,7 @@
     settings = {
         title = "My Awesome Homepage";
         description = "A description of my awesome homepage";
-        allowedHosts = "192.168.0.69:8082";
+        allowedHosts = "http://192.168.0.69:8082/";
     };
   };
   networking.firewall.allowedTCPPorts = [8082];
