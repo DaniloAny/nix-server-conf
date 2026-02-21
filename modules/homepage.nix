026edff2d4ae1730fa2c46578cdaeae939a0e6e7
Main user <daniloany@proton.me>
2026-02-21 10:28:38 +0100

    asdsada3efgerv

--- modules/homepage.nix
+++ modules/homepage.nix
@@ -2,7 +2,10 @@
 {
   services.homepage-dashboard = {
     enable = true;
-    
+    settings = {
+        title = "My Awesome Homepage";
+        description = "A description of my awesome homepage";
+    };
   };
   networking.firewall.allowedTCPPorts = [8082];
   networking.firewall.allowedUDPPorts = [8082];
