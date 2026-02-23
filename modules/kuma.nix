16e3fd67b21ef89018225d7d02cd19904f71661d
Main user <daniloany@proton.me>
2026-02-23 17:01:32 +0100

    something something

--- modules/kuma.nix
+++ modules/kuma.nix
@@ -1,4 +1,10 @@
 { lib, config, pkgs, ...}:
 {
-  services.uptime-kuma.enable = true;
+  services.uptime-kuma = {
+    enable = true;
+    settings = {
+      HOST = "0.0.0.0";
+      PORT = "2000";
+    }
+  };
 }
